310aca9b1a9883f04b750479cac87dd3d2b11065
Kevin Esler <kevin.esler@gmail.com>
2014-07-07 17:23:57 -0400

    Remove cruft

--- foo/src/test/scala/com/timetrade/queueservice/server/testtags/CrashesJenkins.scala
+++
@@ -1,8 +0,0 @@
-package com.timetrade.queueservice.server.testtags
-
-import org.scalatest.Tag
-
-/** Use to tag a test that is a good test and should be run by devlopers,
-  * but that crashes Jenkins until we figure out why.
-  */
-object CrashesJenkins extends Tag("com.timetrade.queueservice.server.testtags.CrashesJenkins")
